ca91466dafe6fddd7171e7bcf1bf9fa54f5fbca7
Randy Setiawan Hoesin <randy.setiawan@bumn.go.id>
2025-04-10 21:27:37 +0700

    Adjustable code for nodemon

--- backend/src/controllers/userController.ts
+++ backend/src/controllers/userController.ts
@@ -1,15 +1,13 @@
+// src/controllers/userController
 import { Request, Response } from 'express';
 import { v4 as uuidv4 } from 'uuid';
 import { User, UserRole } from '../models/User';
 import { users } from '../data/users';
 
-// Handler untuk membuat user baru
 export const createUser = (req: Request, res: Response): Response => {
   const { name, email, role } = req.body;
 
-  // Validasi input
   if (!name || !email || !role) {
-    console.log(name);
     return res.status(400).json({ message: 'Name, email, and role are required.' });
   }
 
@@ -17,7 +15,6 @@ export const createUser = (req: Request, res: Response): Response => {
     return res.status(400).json({ message: 'Role must be either "student" or "teacher".' });
   }
 
-  // Buat user baru
   const newUser: User = {
     id: uuidv4(),
     name,
@@ -25,10 +22,8 @@ export const createUser = (req: Request, res: Response): Response => {
     roles: role as UserRole,
   };
 
-  // Simpan ke array sementara
   users.push(newUser);
 
-  // Response sukses
   return res.status(201).json({
     message: 'User created successfully.',
     user: newUser,
